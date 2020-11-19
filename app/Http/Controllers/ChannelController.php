c0b50c7e7fdcefb33264526390ef300461f42461
nmaiss <nicolas.maissouradze@gmail.com>
2020-11-19 16:58:37 +0100

    channel's name and description modification

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -114,9 +114,11 @@ class ChannelController extends Controller
         return view('channels.admin', ['data' => $channel->all()->where('valid', '0')]);
     }
 
-    public function accept($id){
+    public function accept(Request $req, $id){
         $channel = Channel::find($id);
         $channel->valid = true;
+        $channel->description = $req->input('description');
+        $channel->name = $req->input('name');
         $channel->save();
 
         return redirect()->route('admin');
--- resources/views/channels/admin.blade.php
+++ resources/views/channels/admin.blade.php
@@ -5,13 +5,22 @@
 <div class="container p-5">
     @foreach($data as $el)
         <div class="alert alert-info">
-            <h1>{{ $el->name }}</h1>
-            <a href="https://t.me/"><h5>t.me/{{ $el->url }}</h5></a>
-            <p>{{ $el->description }}</p>
-            <p>{{ $el->members }} membres</p>
-            <p><small>{{ $el->created_at }}</small></p>
-            <a href="{{ route('accept-channel', $el->id) }}"><button class="btn btn-success">Accepter</button></a>
-            <a href="{{ route('reject-channel', $el->id) }}"><button class="btn btn-danger">Rejeter</button></a>
+            <form action="{{ route('accept-channel', $el->id) }}" method="post">
+                @csrf
+
+                <label for="name">
+                    <h1><input value="{{$el->name}}" type="text" name="name" id="name" class="form-control"></h1>
+                </label>
+                <a href="https://t.me/"><h5>t.me/{{ $el->url }}</h5></a>
+                <label for="description">
+                    <textarea name="description" id="description" class="form-control" >{{$el->description}}</textarea>
+                </label>
+
+                <p>{{ $el->members }} membres</p>
+                <p><small>{{ $el->created_at }}</small></p>
+                <a href="{{ route('accept-channel', $el->id) }}"><button class="btn btn-success" type="submit">Accepter</button></a>
+                <a href="{{ route('reject-channel', $el->id) }}"><button class="btn btn-danger">Rejeter</button></a>
+            </form>
         </div>
     @endforeach
 </div>
