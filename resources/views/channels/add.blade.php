034da59f9d19d2545cf058953db8c4c43bc86d41
nmaiss <nicolas.maissouradze@gmail.com>
2020-11-20 21:39:50 +0100

    debug

--- resources/views/channels/add.blade.php
+++ resources/views/channels/add.blade.php
@@ -26,7 +26,7 @@
             </div>
         @endif
 
-        <form action="/add/submit" enctype="multipart/form-data" method="post">
+        <form action="/add/submit" enctype="multipart/form-data" method="get">
             @csrf
 
             <h3 align="center">Ajouter un canal</h3><br />
